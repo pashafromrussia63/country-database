719e5bdd2d04596565260eba03d1d658899b89c8
Pavel Foos <pashafromrussia63@gmail.com>
2022-08-27 14:58:07 +0300

    Add search bar

--- src/Components/CountryList.tsx
+++ src/Components/CountryList.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 import CountryCard,  { Country } from "./CountryCard";
+import { ReactComponent as SearchIcon } from '../icons/search.svg';
 import './CountryList.scss';
 
-function CountryList(props: {countries: Country[]}) {
+function CountryList({ countries } : { countries: Country[] }) {
+  const [filter, setFilter] = useState('');
+
+  const filterCountries = (event : React.ChangeEvent<HTMLInputElement>) : void => {
+    setFilter(event.target.value.toLowerCase());
+  }
+
   return (
     <div className="countryList">
+    <div className="countryList-search">
+      <SearchIcon 
+        className="countryList-searchIcon"
+      />
+      <input
+        placeholder="Search for a country..."
+        onInput = {filterCountries}
+      ></input>
+    </div>
     {
-      props.countries.map(country => {
-        return(
-          <CountryCard
-            key={country.name.common}
-            country={country}
-          />
-        )
+      countries.map(country => {
+        if (country.name.common.toLowerCase().includes(filter)) {
+          return(
+            <CountryCard
+              key={country.name.common}
+              country={country}
+            />
+          )
+        }
+        return null;
       })
     }
     </div>
